678cfdb2ef58b98db164e2d494e79e99b1419f96
Olga <garmashovua@gmail.com>
2026-05-20 08:25:06 +0000

    Refactor frontend to use .js components and update build artifacts

--- octofit-tracker/frontend/src/index.js
+++ octofit-tracker/frontend/src/index.js
@@ -1,1 +1,19 @@
-import "./index.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App";
+
+const codespaceName = import.meta.env.VITE_REACT_APP_CODESPACE_NAME || import.meta.env.REACT_APP_CODESPACE_NAME;
+console.log("OctoFit Tracker frontend starting");
+console.log("Codespace environment name:", codespaceName);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  React.createElement(
+    React.StrictMode,
+    null,
+    React.createElement(BrowserRouter, null, React.createElement(App, null))
+  )
+);
